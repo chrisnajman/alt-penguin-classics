9de4bca3ad11b30da4548231dbbedaf6d1ccb3e4
Chris Najman <christopher.najman@gmail.com>
2025-01-30 16:56:39 +0000

    Simplified sort() function in utils/sort-covers-by-title.js

--- src/utils/sort-covers-by-title.js
+++ src/utils/sort-covers-by-title.js
@@ -1,11 +1,5 @@
 export const sortCoversByTitle = (covers) => {
-  return covers.sort((a, b) => {
-    if (a.title.toLowerCase() < b.title.toLowerCase()) {
-      return -1
-    }
-    if (a.title.toLowerCase() > b.title.toLowerCase()) {
-      return 1
-    }
-    return 0
-  })
+  return covers.sort((a, b) =>
+    a.title.toLowerCase().localeCompare(b.title.toLowerCase())
+  )
 }
